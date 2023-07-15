baebb6855b8ac87136b9a5131825fb7990984844
Olawumi Oluwafemi Steven <freshfemi1990@gmail.com>
2023-07-15 19:03:06 +0100

    EASY WAY TO DESIGN A PORTFOLIO

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ================================= typing animation ================================ */
+var typed = new Typed(".typing", {
+    strings:["", "Web Designer", "Front End", "Professional BugBounter", ""] 
+    FastSpeed:100,
+    BackSpeed:60,
+    loop:true,
+})
